3754e22af43a9834d59d8b38c2431c879730efed
EricPfeuti <eric.pfeuti2007@gmail.com>
2025-05-22 11:03:04 -0300

    Criptografa - Aula Redes

---
+++ criptografiaRedes/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'criptografiaRedes',
+  webDir: 'www'
+};
+
+export default config;
