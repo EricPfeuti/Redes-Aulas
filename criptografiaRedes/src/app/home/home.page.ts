e4063761636f16cd1bec847e66a55631f6ce850d
EricPfeuti <eric.pfeuti2007@gmail.com>
2025-05-22 11:17:37 -0300

    Alterando arquivos

--- criptografiaRedes/src/app/home/home.page.ts
+++ criptografiaRedes/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,28 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-
   constructor() {}
 
+  message: string = '';
+  secretKey: string = '';
+  encryptedMessage: string = '';
+  decryptedMessage: string = '';
+
+  encryptMessage() {
+    if (this.message && this.secretKey) {
+      const encrypted = CryptoJS.AES.encrypt(
+        this.message,
+        this.secretKey
+      ).toString();
+      this.encryptedMessage = encrypted;
+    }
+  }
+
+  decryptMessage() {
+    if (this.encryptedMessage && this.secretKey) {
+      const bytes = CryptoJS.AES.decrypt(this.encryptedMessage, this.secretKey);
+      const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+      this.decryptedMessage = decrypted;
+    }
+  }
 }
